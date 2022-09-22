2c5e80e97d57045de8bee19587e80802e09e682d
Karen Cano <karen.fernandes.cano@gmail.com>
2022-09-22 18:41:52 -0300

    fix: corrige rota '/'

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,7 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import LoginProvider from './context/LoginContext';
 import login from './pages/login';
-// import Register from './pages/Register';
+import Register from './pages/Register';
 import ProductsProvider from './context/ProductsContext';
 import Products from './pages/Products';
 
@@ -13,11 +13,11 @@ class App extends Component {
       <Switch>
         <LoginProvider>
           <Route exact path="/login" component={ login } />
+          <Redirect from="/" to="/login" />
           <Route exact path="/customer/products" component={ Products } />
         </LoginProvider>
         <ProductsProvider />
-        {/* <Route exact path="/register" component={ Register } /> */}
-        <Redirect from="/" to="/login" />
+        <Route exact path="/register" component={ Register } />
       </Switch>
     );
   }

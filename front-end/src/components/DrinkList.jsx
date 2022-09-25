292af6810c3b13d23b7cd169891b4ade2b89c1ae
Kevin Willian <kevinwillian084@gmail.com>
2022-09-24 21:19:51 -0300

    perf: Tira a responsabilidade de fazer a requisicao para a API, pegando os dados diretamente do ProductsContext
    Nome do arquivo

---
+++ front-end/src/components/DrinkList.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useProductsContext } from '../context/ProductsContext';
+import '../styles/pages/products.css';
+import Counter from './Counter';
+
+function DrinkList() {
+  const { drinks, loading } = useProductsContext();
+  return (
+    <div className="drink-container">
+      {loading ? <p>Loading...</p> : drinks.map((drink) => (
+        <div key={ drink.id } className="drink-card">
+          <p data-testid={ `customer_products__element-card-price-${drink.id}` }>
+            {'R$ '}
+            {drink.price}
+          </p>
+          <img
+            src={ drink.urlImage }
+            alt={ drink.name }
+            data-testid={ `customer_products__img-card-bg-image-${drink.id}` }
+          />
+          <p
+            data-testid={ `customer_products__element-card-title-${drink.id}` }
+          >
+            {drink.name}
+          </p>
+          <Counter
+            drink={ drink }
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default DrinkList;

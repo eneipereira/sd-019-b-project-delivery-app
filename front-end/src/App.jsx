52d2ab8bf68a5bcbafbe23cf2ee2b3b660c202d9
Kevin Willian <kevinwillian084@gmail.com>
2022-09-24 22:40:20 -0300

    refactor: Troca App de classe para componente funcional e adiciona hook para redirecionar para a pagina de login

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { Switch, Route, Redirect, useRouteMatch } from 'react-router-dom';
 import './App.css';
 import LoginProvider from './context/LoginContext';
 import Login from './pages/Login';
@@ -9,23 +9,23 @@ import Products from './pages/Products';
 import Checkout from './pages/Checkout';
 import Header from './components/Header';
 
-class App extends Component {
-  render() {
-    return (
-      <Switch>
-        <ProductsProvider>
-          <LoginProvider>
-            <Route exact path="/" component={ Login } />
-            <Route exact path="/login" component={ Login } />
-            <Route path="/customer/" component={ Header } />
-            <Route exact path="/customer/products" component={ Products } />
-            <Route exact path="/customer/checkout" component={ Checkout } />
-            <Route exact path="/register" component={ Register } />
-          </LoginProvider>
-        </ProductsProvider>
-      </Switch>
-    );
-  }
+function App() {
+  const match = useRouteMatch('/');
+  return (
+    <Switch>
+      <ProductsProvider>
+        <LoginProvider>
+          {match.isExact && <Redirect to="/login" />}
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/login" component={ Login } />
+          <Route path="/customer/" component={ Header } />
+          <Route exact path="/customer/products" component={ Products } />
+          <Route exact path="/customer/checkout" component={ Checkout } />
+          <Route exact path="/register" component={ Register } />
+        </LoginProvider>
+      </ProductsProvider>
+    </Switch>
+  );
 }
 
 export default App;

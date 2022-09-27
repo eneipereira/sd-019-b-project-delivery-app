fd679fb946a7e9b54a36b97e739a734abdbb223a
Kevin Willian <kevinwillian084@gmail.com>
2022-09-26 23:57:09 -0300

    feat: Desacopla funcao de manipular o array de produtos do provider, e adiciona ao componente de responsavel por renderizar a lista

--- front-end/src/context/ProductsContext.jsx
+++ front-end/src/context/ProductsContext.jsx
@@ -17,23 +17,13 @@ function ProductsProvider({ children }) {
   const [drinks, setDrinks] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  // Pega os dados da API e em seguida verifica se há algum dado no localStorage,
-  // se houver, ele faz um map no retorno da API e verifica se o id do item é existente no array do localStorage, se não for, ele retorna o item da API, senão, ele retorna o item do localStorage.
+  //  Seta os dados iniciais que retornam da API
   // Refatorar ****
   useEffect(() => {
     async function fetchDrinks() {
       const response = await fetch('http://localhost:3001/products');
       const data = await response.json();
-      const cartStorage = getLocalStorageParsed('cart', []);
-      const drinksStorage = data.map((drink) => {
-        const cartStorageIndex = cartStorage.findIndex((item) => item.id === drink.id);
-        if (cartStorageIndex < 0) {
-          drink.quantity = 0;
-          return drink;
-        }
-        return cartStorage[cartStorageIndex];
-      });
-      setDrinks(drinksStorage);
+      setDrinks(data);
       setLoading(false);
     }
     fetchDrinks();

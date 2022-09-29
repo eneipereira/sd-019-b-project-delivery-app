e1198b2152969bc7fd665d9ba2ee614f35ea8f43
Janiele Vilar <vilarjaniele@gmail.com>
2022-09-29 17:35:58 -0300

    Tentando requisitos 33 e 34

--- front-end/src/components/SaleDetailsCard.jsx
+++ front-end/src/components/SaleDetailsCard.jsx
@@ -1,42 +1,66 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import api from '../services';
 import { serializeDate, serializePrice } from '../utils';
 
 function SaleDetailsCard() {
   const testId = 'seller_order_details__';
   const { id } = useParams();
   const [saleState, setSaleState] = useState([]);
+  const [disableBtn1, setDisableBtn1] = useState(true);
+  const [disableBtn2, setDisableBtn2] = useState(true);
 
   useEffect(() => {
+    const disableButton = (status) => {
+      if (status === 'Pendente') {
+        setDisableBtn1(false);
+      } else if (status === 'Preparando') {
+        setDisableBtn2(false);
+      }
+    };
+
     const fetchSale = async () => {
       const response = await fetch(`http://localhost:3001/orders/${id}`);
       const data = await response.json();
+      disableButton(data[0].status);
       setSaleState(data);
     };
     fetchSale();
   }, [id]);
+
+  const setOrderStatus = (status) => {
+    api.patch(`/orders/${id}`, { status })
+      .then()
+      .catch((err) => err.response.data);
+  };
 
   return (
     <div>
       {!saleState.length ? <p>Loading...</p>
         : saleState.map((sale) => (
           <div key={ sale.id }>
-            <h1 data-testId={ `${testId}element-order-details-label-order-id` }>
+            <h1 data-testid={ `${testId}element-order-details-label-order-id` }>
               {sale.id}
             </h1>
-            <p data-testId={ `${testId}element-order-details-label-order-date` }>
+            <p data-testid={ `${testId}element-order-details-label-order-date` }>
               {serializeDate(sale.saleDate)}
             </p>
             <p data-testid={ `${testId}element-order-details-label-delivery-status` }>
               {sale.status}
             </p>
-            <button type="button" data-testid={ `${testId}button-preparing-check` }>
+            <button
+              type="button"
+              disabled={ disableBtn1 }
+              data-testid={ `${testId}button-preparing-check` }
+              onClick={ () => setOrderStatus('Preparando') }
+            >
               Preparar Pedido
             </button>
             <button
               type="button"
-              disabled
+              disabled={ disableBtn2 }
               data-testid={ `${testId}button-dispatch-check` }
+              onClick={ () => setOrderStatus('Em Trânsito') }
             >
               Saiu para Entrega
             </button>

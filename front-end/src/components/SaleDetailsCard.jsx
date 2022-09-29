d616cb1d942724c19016ee780d73a9e85c92f5b4
Kevin Willian <kevinwillian084@gmail.com>
2022-09-29 02:17:42 -0300

    refactor: Refatora constantes testId

--- front-end/src/components/SaleDetailsCard.jsx
+++ front-end/src/components/SaleDetailsCard.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import { serializeDate, serializePrice } from '../utils';
 
 function SaleDetailsCard() {
-  const dataId = 'seller_order_details__';
+  const testId = 'seller_order_details__';
   const { id } = useParams();
   const [saleState, setSaleState] = useState([]);
 
@@ -21,47 +21,47 @@ function SaleDetailsCard() {
       {!saleState.length ? <p>Loading...</p>
         : saleState.map((sale) => (
           <div key={ sale.id }>
-            <h1 data-testId={ `${dataId}element-order-details-label-order-id` }>
+            <h1 data-testId={ `${testId}element-order-details-label-order-id` }>
               {sale.id}
             </h1>
-            <p data-testId={ `${dataId}element-order-details-label-order-date` }>
+            <p data-testId={ `${testId}element-order-details-label-order-date` }>
               {serializeDate(sale.saleDate)}
             </p>
-            <p data-testid={ `${dataId}element-order-details-label-delivery-status` }>
+            <p data-testid={ `${testId}element-order-details-label-delivery-status` }>
               {sale.status}
             </p>
-            <button type="button" data-testid={ `${dataId}button-preparing-check` }>
+            <button type="button" data-testid={ `${testId}button-preparing-check` }>
               Preparar Pedido
             </button>
             <button
               type="button"
               disabled
-              data-testid={ `${dataId}button-dispatch-check` }
+              data-testid={ `${testId}button-dispatch-check` }
             >
               Saiu para Entrega
             </button>
             <div>
               {sale.products.map((item, index) => (
                 <div key={ item.id } className="salecard-card">
-                  <p data-testid={ `${dataId}element-order-table-item-number-${index}` }>
+                  <p data-testid={ `${testId}element-order-table-item-number-${index}` }>
                     {index + 1}
                   </p>
-                  <p data-testid={ `${dataId}element-order-table-name-${index}` }>
+                  <p data-testid={ `${testId}element-order-table-name-${index}` }>
                     {item.name}
                   </p>
-                  <p data-testid={ `${dataId}element-order-table-quantity-${index}` }>
+                  <p data-testid={ `${testId}element-order-table-quantity-${index}` }>
                     {item.prodQty.quantity}
                   </p>
-                  <p data-testid={ `${dataId}element-order-table-unit-price-${index}` }>
+                  <p data-testid={ `${testId}element-order-table-unit-price-${index}` }>
                     {serializePrice(item.price)}
                   </p>
-                  <p data-testid={ `${dataId}element-order-table-sub-total-${index}` }>
+                  <p data-testid={ `${testId}element-order-table-sub-total-${index}` }>
                     {serializePrice((item.price * item.prodQty.quantity).toFixed(2))}
                   </p>
                 </div>
               ))}
             </div>
-            <p data-testid={ `${dataId}element-order-total-price` }>
+            <p data-testid={ `${testId}element-order-total-price` }>
               {serializePrice(saleState[0].totalPrice)}
             </p>
           </div>
